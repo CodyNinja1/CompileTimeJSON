cf7919bc51fa2e3816d96459c9a33bada68009f4
CodyNinja1 <jailmanguy1@gmail.com>
2026-05-23 21:13:22 +0300

    Add arrays/vectors, and pointer null

--- src/json.h
+++ src/json.h
@@ -1,19 +1,51 @@
 #include <meta>
 #include <string>
+
+// TODO: Make sure which one of these puts std::begin/end/size and nothing extra
 #include <array>
 #include <vector>
+#include <iterator>
+#include <ranges>
 
 namespace CompileTimeJSON
 {
     template <typename T>
     concept ToStringable =
-        requires(T v) {
-            { std::to_string(v) } -> std::convertible_to<std::string>;
-        };
-
-    template <typename ValueT, size_t Indent = 4> constexpr auto StructToJSON(const ValueT& Value)
+        requires(T v) 
     {
-        if constexpr (ToStringable<ValueT>)
+        { std::to_string(v) } -> std::convertible_to<std::string>;
+    };
+
+    template<typename T>
+    concept HasBeginEnd =
+        requires(T t)
+    {
+        std::begin(t);
+        std::end(t);
+    };
+
+    template <typename ValueT> constexpr std::string ToJSON(ValueT Value, size_t Indent = 4)
+    {
+        constexpr bool IsRefToClass = [] {
+            if constexpr (std::is_reference_v<ValueT>)
+            {
+                if constexpr (std::is_class_v<typename [:std::meta::remove_reference(^^ValueT):]>)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }();
+
+        if constexpr (std::is_pointer_v<ValueT>)
+        {
+            return Value ? ToJSON<decltype(*Value)>(*Value, Indent) : "null";
+        }
+        else if constexpr (std::is_same_v<ValueT, bool>)
+        {
+            return Value ? "true" : "false";
+        }
+        else if constexpr (ToStringable<ValueT>)
         {
             return std::to_string(Value);
         }
@@ -25,8 +57,49 @@ namespace CompileTimeJSON
         {
             return '"' + std::string(Value) + '"';
         }
-        else
+        else if constexpr (HasBeginEnd<ValueT>)
         {
+            std::string IndentStr = "";
+            std::string IndentStrMinusFour = "";
+
+            for (size_t Idx = 0; Idx < Indent; Idx++)
+            {
+                IndentStr += " ";
+            }
+
+            for (size_t Idx = 0; Idx < Indent - 4; Idx++)
+            {
+                IndentStrMinusFour += " ";
+            }
+
+            std::string OutputResult = "[\n";
+
+            size_t MemberIdx = 0;
+            for (auto Child : Value)
+            {
+                OutputResult +=
+                IndentStr
+                + ToJSON<decltype(Child)>(Child, Indent + 4)
+                + (MemberIdx == std::size(Value) - 1 ? "\n" : ",\n");
+
+                MemberIdx++;
+            }
+
+            return OutputResult + IndentStrMinusFour + "]";
+        }
+        else if constexpr ((std::is_class_v<ValueT> or IsRefToClass) and not HasBeginEnd<ValueT>)
+        {
+            constexpr std::meta::info ValueTInfo = []{
+                if constexpr (std::is_reference_v<ValueT>)
+                {
+                    return std::meta::remove_reference(^^ValueT);
+                }
+                else
+                {
+                    return ^^ValueT;
+                }
+            }();
+
             std::string IndentStr = "";
             std::string IndentStrMinusFour = "";
 
@@ -42,7 +115,7 @@ namespace CompileTimeJSON
 
             std::string OutputResult = "{\n";
 
-            constexpr auto Members = std::define_static_array(std::meta::nonstatic_data_members_of(^^ValueT, std::meta::access_context::current()));
+            constexpr auto Members = std::define_static_array(std::meta::nonstatic_data_members_of(ValueTInfo, std::meta::access_context::current()));
 
             size_t MemberIdx = 0;
             template for (constexpr std::meta::info Member : auto(Members))
@@ -52,14 +125,17 @@ namespace CompileTimeJSON
                 + '"' 
                 + std::string(std::meta::identifier_of(Member)) 
                 + "\": " 
-                + StructToJSON<decltype(Value.[:Member:]), Indent + 4>(Value.[:Member:]) 
-                + (MemberIdx == Members.size() - 1 ? "" : ",")
-                + "\n";
+                + StructToJSON<decltype(Value.[:Member:])>(Value.[:Member:], Indent + 4) 
+                + (MemberIdx == std::size(Members) - 1 ? "\n" : ",\n");
 
                 MemberIdx++;
             }
 
             return OutputResult + IndentStrMinusFour + "}";
+        }
+        else
+        {
+            return std::string("<invalid>");
         }
     }
 }
